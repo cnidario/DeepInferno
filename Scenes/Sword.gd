71442219322ca27bddf3941131507c1851aa16da
Cni <cnidario@cnidaria.org>
2020-04-19 18:28:26 +0200

    attack fix, no body entered, check every process loop

--- Scenes/Sword.gd
+++ Scenes/Sword.gd
@@ -4,8 +4,10 @@ onready var animation_player = $AnimationPlayer
 onready var animation_tree = $AnimationTree
 onready var state_machine = $AnimationTree.get("parameters/playback")
 onready var peace_timer = $PeaceTimer
+onready var area2D = $Area2D
 
 var attacking = false
+var attacked = []
 
 func _ready():
 	state_machine.start("idle")
@@ -15,19 +17,24 @@ func attack():
 	state_machine.travel("attack")
 	peace_timer.start()
 
-func _physics_process(delta):
-	pass
 func attack_peak():
 	attacking = false
+	attacked = []
 func attack_started():
 	attacking = true
 	
 func is_on_hit_yaxis(body):
 	var ydst = global_position.y - body.global_position.y
 	return ydst <0 and ydst > -8
+	
+func canAttack(body):
+	return !body in attacked and body.name == "Demon" and is_on_hit_yaxis(body)
 
-func on_body_entered(body):
+func _process(delta):
 	if attacking:
-		print(attacking)
-		if body.name == "Demon" and is_on_hit_yaxis(body):
-			body.getHit()
+		var bodies = area2D.get_overlapping_bodies()
+		for body in bodies:
+			if canAttack(body):
+				body.getHit()
+				attacked.append(body)
+
