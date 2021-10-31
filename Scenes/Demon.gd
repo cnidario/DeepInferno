58b7979582d4ba35d101069755feb5ec25da03aa
Maria D <nospam@porfa.com>
2021-10-31 17:39:42 +0100

    godot changes

--- Scenes/Demon.gd
+++ Scenes/Demon.gd
@@ -2,6 +2,7 @@ extends KinematicBody2D
 
 onready var hp_bar = $HPBar
 onready var animation_player = $AnimationPlayer
+onready var player_radar = $PlayerRadar
 
 func _ready():
 	pass
@@ -10,3 +11,5 @@ func getHit(damage = 10):
 	hp_bar.setHP(hp_bar.hp - damage)
 	animation_player.play("hit")
 	
+#func _process(delta):
+#	player_radar.
